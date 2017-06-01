1c623be10b1a1e3686e367aa269209e97ea2d0eb
torbjorn.torbjornsen <ttorbjornsen@gmail.com>
2017-06-01 23:29:56 +0200

    init

--- hw1_regression.py
+++ hw1_regression.py
@@ -1,7 +1,30 @@
 import sys
+import os
+import pandas as pd
+import numpy as np
 
-inputLambda = sys.argv[1]
-inputSigma = sys.argv[2]
+inputLambda = float(sys.argv[1])
+inputSigma = float(sys.argv[2])
 
-print inputLambda
-print inputSigma
+print 'lambda: ' + str(inputLambda)
+print 'sigma: ' + str(inputSigma)
+
+
+scriptDir = os.path.dirname(__file__) #<-- absolute dir the script is in
+xTrainPath = os.path.join(scriptDir, sys.argv[3])
+yTrainPath = os.path.join(scriptDir, sys.argv[4])
+X = pd.read_csv(xTrainPath, header=None)
+y = pd.read_csv(yTrainPath, header=None)
+d = X.shape[0]
+
+identityMatrix = np.identity(d)
+temp = inputLambda*identityMatrix
+X_T = pd.DataFrame.transpose(X)
+
+firstTerm = (pd.DataFrame.dot(X,X_T) + inputLambda*identityMatrix)
+print firstTerm
+firstTermInverse = pd.DataFrame(np.linalg.pinv(firstTerm.values), firstTerm.columns, firstTerm.index)
+print firstTermInverse
+
+
+
