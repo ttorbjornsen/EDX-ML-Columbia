9aa0820f67739ef0054005f83edb5f4c9b215d1a
torbjorn.torbjornsen <ttorbjornsen@gmail.com>
2017-06-04 23:21:25 +0200

    started active learning

--- hw1_regression.py
+++ hw1_regression.py
@@ -14,24 +14,52 @@ inputSigma = parseInput(sys.argv[2])
 
 
 scriptDir = os.path.dirname(__file__) #<-- absolute dir the script is in
-targetFileRelPath = 'wRR_' + str(inputLambda) + '.csv'
-targetFileAbsPath = os.path.join(scriptDir, targetFileRelPath)
+rrTargetFileRelPath = 'wRR_' + str(inputLambda) + '.csv'
+rrTargetFileAbsPath = os.path.join(scriptDir, rrTargetFileRelPath)
+
+alTargetFileRelPath = 'active_' + str(inputLambda) + '_' + str(inputSigma) + '.csv'
+altargetFileAbsPath = os.path.join(scriptDir, rrTargetFileRelPath)
 
 scriptDir = os.path.dirname(__file__) #<-- absolute dir the script is in
 xTrainPath = os.path.join(scriptDir, sys.argv[3])
 yTrainPath = os.path.join(scriptDir, sys.argv[4])
+xTestPath = os.path.join(scriptDir, sys.argv[5])
+
 X = pd.read_csv(xTrainPath, header=None)
 y = pd.read_csv(yTrainPath, header=None)
 d = X.shape[1] #number of dimensions (feaatures)
 
+# PART 1 - Ridge regression
 identityMatrix = np.identity(d)
 X_T = X.transpose()
-
 leastSquares = (np.linalg.inv(X_T.dot(X))).dot(X_T).dot(y)
 ridgeRegression = (np.linalg.inv(inputLambda*identityMatrix + X_T.dot(X))).dot(X_T).dot(y)
-
 dfRidgeRegression = pd.DataFrame(ridgeRegression)
-dfRidgeRegression.to_csv(targetFileAbsPath, sep=';', header=None, index=False)
+dfRidgeRegression.to_csv(rrTargetFileAbsPath, sep=';', header=None, index=False)
+
+
+# PART 2 - Active learning
+XTest = pd.read_csv(xTestPath, header=None)
+covarianceSigmaMatrix = np.linalg.inv(inputLambda * identityMatrix + ((X_T.dot(X))/inputSigma))
+
+
+
+indexTraversalIndex = []
+for i in range(1,10): #find the 10 highest variances
+    highestVariance = -1
+    tempIndex = -1
+    for xTestRow in range(0, XTest.shape[0]):
+        x0 = XTest.iloc[[xTestRow]]
+        x0_T = x0.transpose()
+        sigma0 = inputSigma + x0.dot(covarianceSigmaMatrix).dot(x0_T).iloc[0][xTestRow]
+
+        if (sigma0 > highestVariance) and (xTestRow+1 not in indexTraversalIndex):
+            highestVariance = sigma0
+            tempIndex = xTestRow + 1
+
+    indexTraversalIndex.append(tempIndex)
+
+print indexTraversalIndex
 
 
 
